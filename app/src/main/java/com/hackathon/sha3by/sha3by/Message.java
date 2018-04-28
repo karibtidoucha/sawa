9a8d228575a538624284dd0a28fd7d6965202827
NadahF <nadahfeteih@gmail.com>
2018-04-28 15:32:04 +0400

    Added messages xml and class

--- app/src/main/java/com/hackathon/sha3by/sha3by/Message.java
+++ app/src/main/java/com/hackathon/sha3by/sha3by/Message.java
@@ -1,4 +1,34 @@
 package com.hackathon.sha3by.sha3by;
 
 public class Message {
+
+    private String messageText;
+    private String messageUser;
+
+    public Message(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+    }
+
+    public Message(){
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
 }
