d26a6d2c6cf03d447110235a6db53eb38bf18c36
msn307 <msn307@nyu.edu>
2018-04-28 16:06:50 +0400

    2.Muna.3

--- app/src/main/java/com/hackathon/sha3by/sha3by/Activity3.java
+++ app/src/main/java/com/hackathon/sha3by/sha3by/Activity3.java
@@ -2,12 +2,20 @@ package com.hackathon.sha3by.sha3by;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Activity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity2);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity3);
     }
 }
--- app/src/main/java/com/hackathon/sha3by/sha3by/MainActivity.java
+++ app/src/main/java/com/hackathon/sha3by/sha3by/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.second_activity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
