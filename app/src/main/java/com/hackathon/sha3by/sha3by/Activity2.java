09f8267122961488ab41aa85e7fde3eef8bc86cd
msn307 <msn307@nyu.edu>
2018-04-28 14:01:07 +0400

    2.Muna.2

--- app/src/main/java/com/hackathon/sha3by/sha3by/Activity2.java
+++ app/src/main/java/com/hackathon/sha3by/sha3by/Activity2.java
@@ -1,23 +1,26 @@
 package com.hackathon.sha3by.sha3by;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity2);
 
-        Button button = findViewById(R.id.second_activity);
+        Button button = findViewById(R.id.third_activity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Activity2.class));
+                startActivity(new Intent(Activity2.this, Activity3.class));
             }
         });
     }
