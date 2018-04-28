254add851753d6b40ba4c8ebf9b711abe30cb55f
NadahF <nadahfeteih@gmail.com>
2018-04-28 16:16:46 +0400

    added messages

--- app/src/main/java/com/hackathon/sha3by/sha3by/Activity3.java
+++ app/src/main/java/com/hackathon/sha3by/sha3by/Activity3.java
@@ -2,6 +2,12 @@ package com.hackathon.sha3by.sha3by;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Activity3 extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
+
+        Button fab =
+                (Button)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                MessageStore.getInstance
+                        .push()
+                        .setValue(new Message(input.getText().toString(),
+                                FirebaseApp.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
+            }
+        });
     }
 }
